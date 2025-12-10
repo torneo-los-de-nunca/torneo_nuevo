d2043f3135c9e930170e869ecf2450551098e737
Ezequiel Dietta <tuemail@ejemplo.com>
2025-12-10 17:30:14 -0300

    Fix: import Firebase case-sensitive for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
   deleteDoc
 } from "firebase/firestore";
 
-import { db } from "./firebase";
+import { db } from "./Firebase";
 
 // ===============================
 // CONFIGURACIÓN BÁSICA
